8980b77b110743feb94d367c9415910a90568f8f
ALANN <148600292+RainAllln@users.noreply.github.com>
2024-11-29 19:33:02 +0800

    modify structure

---
+++ src/main/java/team/jnu/wardsystem/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package team.jnu.wardsystem.mapper;
+
+import team.jnu.wardsystem.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> selectAll();
+
+}
---
+++ src/main/java/team/jnu/wardsystem/pojo/User.java
@@ -0,0 +1,13 @@
+package team.jnu.wardsystem.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String username;
+    private String password;
+
+
+}
