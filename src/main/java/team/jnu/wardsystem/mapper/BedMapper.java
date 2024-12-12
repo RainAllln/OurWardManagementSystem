306b2e102d2448bbc46f76cb52324d3cb2314c7b
ALANN <148600292+RainAllln@users.noreply.github.com>
2024-12-12 11:05:43 +0800

    update clean to help

--- src/main/java/team/jnu/wardsystem/mapper/BedMapper.java
+++ src/main/java/team/jnu/wardsystem/mapper/BedMapper.java
@@ -7,7 +7,7 @@ import team.jnu.wardsystem.pojo.Bed;
 public interface BedMapper {
     void updateBedStatus(@Param("bed_id") int bedId, @Param("ward_id") int wardId, @Param("in_use") boolean b);
 
-    int updateBedstatus(@Param("bed_id") int bedId, @Param("ward_id") int wardId, @Param("clean") boolean b);
+    int updateBedstatus(@Param("bed_id") int bedId, @Param("ward_id") int wardId, @Param("help") boolean b);
 
     List<Bed> searchAllBed(int nurse_id);
 
